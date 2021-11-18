e1081ffdf748e9cacadb1317c1e66c8bb9ca626d
deanxx <deanxx999@gmail.com>
2021-11-18 16:27:12 +0900

    add class info controller

---
+++ adams-repository-service/Controllers/ClassInfoController.cs
@@ -0,0 +1,52 @@
+﻿using adams_repository_service.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using NAVIAIServices.RepositoryService;
+using NAVIAIServices.RepositoryService.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace adams_repository_service.Controllers
+{
+    [Route("classinfos")]
+    [ApiController]
+    public class ClassInfoController : ControllerBase
+    {
+        IRepositoryService _repositoryService;
+        private string _DbRoot;
+
+        public ClassInfoController(IRepositoryService repositoryService, IConfiguration configuration)
+        {
+            _DbRoot = configuration.GetValue<string>("DbRoot");
+            _repositoryService = repositoryService;
+        }
+
+        [HttpGet("{projectId}")]
+        public ActionResult GetAllClassInfo(string projectId)
+        {
+            var dbPath = System.IO.Path.Combine(_DbRoot, projectId + ".db");
+            var projectService = _repositoryService.GetProjectService(dbPath, DBType.LiteDB);
+            var classInfo = projectService.ClassInfos.FindAll().ToList();
+            return Ok(classInfo);
+        }
+
+        [HttpPost("{projectId}")]
+        public ActionResult CreateClassInfo(string projectId, [FromBody] CreateClassInfoModel createClassInfoModel)
+        {
+            var entity = new ClassInfo(
+                createClassInfoModel.Name,
+                createClassInfoModel.Description,
+                createClassInfoModel.R,
+                createClassInfoModel.G,
+                createClassInfoModel.B,
+                createClassInfoModel.IsEnabled
+                );
+            var dbPath = System.IO.Path.Combine(_DbRoot, projectId + ".db");
+            var projectService = _repositoryService.GetProjectService(dbPath, DBType.LiteDB);
+            projectService.ClassInfos.Add(entity);
+            return Ok(entity);
+        }
+    }
+}
---
+++ adams-repository-service/Models/CreateClassInfoModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace adams_repository_service.Models
+{
+    public class CreateClassInfoModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public byte R { get; set; }
+        public byte G { get; set; }
+        public byte B { get; set; }
+        public bool IsEnabled { get; set; }
+
+        public CreateClassInfoModel(string name, string Description, byte R, byte G, byte B, bool isEnabled = true)
+        {
+            this.Name = name;
+            this.IsEnabled = isEnabled;
+            this.Description = Description;
+            this.R = R;
+            this.G = G;
+            this.B = B;
+        }
+
+        public CreateClassInfoModel()
+        {
+
+        }
+    }
+}
